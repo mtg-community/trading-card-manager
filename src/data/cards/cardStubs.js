9c824d46dbc6f06f9d7b907a5baf317ad0140d5a
Eduardo Moroni <eduardomoroni@gmail.com>
2018-07-09 06:35:00 -0300

    feat: add card stubs

---
+++ src/data/cards/cardStubs.js
@@ -0,0 +1,312 @@
+export const lordOfAtlantis = {
+  multiverseid: 131676,
+  artist: 'Melissa A. Benson',
+  cmc: 2,
+  colorIdentity: ['U'],
+  colors: ['Blue'],
+  flavor:
+    'A master of tactics, the Lord of Atlantis makes his people bold in battle merely by arriving to lead them.',
+  foreignNames: [
+    {
+      language: 'Chinese Simplified',
+      name: '亚特兰提斯领主',
+    },
+    {
+      language: 'French',
+      name: "Seigneur de l'Atlantide",
+    },
+    {
+      language: 'German',
+      name: 'Herrscher von Atlantis',
+    },
+    {
+      language: 'Italian',
+      name: 'Signore di Atlantide',
+    },
+    {
+      language: 'Japanese',
+      name: 'アトランティスの王',
+    },
+    {
+      language: 'Portuguese (Brazil)',
+      name: 'Senhor da Atlântida',
+    },
+    {
+      language: 'Russian',
+      name: 'Лорд Атлантиды',
+    },
+    {
+      language: 'Spanish',
+      name: 'Señor de la Atlántida',
+    },
+  ],
+  id: 'a5c973cc599dc78dfbab99a4fd4038a9c3c535da',
+  imageName: 'lord of atlantis',
+  layout: 'normal',
+  legalities: [
+    {
+      format: 'Commander',
+      legality: 'Legal',
+    },
+    {
+      format: 'Legacy',
+      legality: 'Legal',
+    },
+    {
+      format: 'Modern',
+      legality: 'Legal',
+    },
+    {
+      format: 'Time Spiral Block',
+      legality: 'Legal',
+    },
+    {
+      format: 'Vintage',
+      legality: 'Legal',
+    },
+  ],
+  manaCost: '{U}{U}',
+  mciNumber: '3',
+  name: 'Lord of Atlantis',
+  number: '3',
+  power: '2',
+  printings: [
+    'LEA',
+    'LEB',
+    '2ED',
+    'CED',
+    'CEI',
+    '3ED',
+    '4ED',
+    '5ED',
+    '6ED',
+    'pSUS',
+    '7ED',
+    'TSB',
+  ],
+  rarity: 'Special',
+  releaseDate: '2000-12-01',
+  source: 'Junior Super Series participation bonus.',
+  subtypes: ['Merfolk'],
+  text:
+    "Other Merfolk creatures get +1/+1 and have islandwalk. (They can't be blocked as long as defending player controls an Island.)",
+  toughness: '2',
+  type: 'Creature — Merfolk',
+  types: ['Creature'],
+};
+
+export const liliana = {
+  multiverseid: 272761,
+  artist: 'Steve Argyle',
+  cmc: 3,
+  colorIdentity: ['B'],
+  colors: ['Black'],
+  foreignNames: [
+    {
+      language: 'Chinese Simplified',
+      name: '面纱的莉莲娜',
+    },
+    {
+      language: 'Chinese Traditional',
+      name: '面紗的莉蓮娜',
+    },
+    {
+      language: 'French',
+      name: 'Liliana du voile',
+    },
+    {
+      language: 'German',
+      name: 'Liliana mit Schleier',
+    },
+    {
+      language: 'Italian',
+      name: 'Liliana del Velo',
+    },
+    {
+      language: 'Japanese',
+      name: 'ヴェールのリリアナ',
+    },
+    {
+      language: 'Portuguese (Brazil)',
+      name: 'Liliana do Véu',
+    },
+    {
+      language: 'Russian',
+      name: 'Лилиана с Завесой',
+    },
+    {
+      language: 'Spanish',
+      name: 'Liliana del Velo',
+    },
+  ],
+  id: 'deaab81889362d156eb6fe190a49bb27d4858cd6',
+  imageName: 'liliana of the veil',
+  layout: 'normal',
+  legalities: [
+    {
+      format: 'Commander',
+      legality: 'Legal',
+    },
+    {
+      format: 'Innistrad Block',
+      legality: 'Legal',
+    },
+    {
+      format: 'Legacy',
+      legality: 'Legal',
+    },
+    {
+      format: 'Modern',
+      legality: 'Legal',
+    },
+    {
+      format: 'Vintage',
+      legality: 'Legal',
+    },
+  ],
+  loyalty: 3,
+  manaCost: '{1}{B}{B}',
+  mciNumber: '4',
+  name: 'Liliana of the Veil',
+  number: '4',
+  printings: ['ISD', 'pWCQ', 'MM3'],
+  rarity: 'Special',
+  rulings: [
+    {
+      date: '2017-03-14',
+      text:
+        "When Liliana's first ability resolves, first the player whose turn it is chooses a card to discard, then each other player in turn order chooses a card to discard, then those cards are discarded simultaneously. No one sees what the other players are discarding before deciding which card to discard.",
+    },
+    {
+      date: '2017-03-14',
+      text:
+        "The player targeted by Liliana's second ability chooses which creature to sacrifice when the ability resolves. This ability doesn't target any creature.",
+    },
+    {
+      date: '2017-03-14',
+      text:
+        "When Liliana's third ability resolves, you put each permanent the player controls into one of the two piles. For example, you could put a creature into one pile and an Aura enchanting that creature into the other pile.",
+    },
+    {
+      date: '2017-03-14',
+      text:
+        'A pile can be empty. If the player chooses an empty pile, no permanents will be sacrificed.',
+    },
+  ],
+  source: 'Regional PTQ 2015',
+  subtypes: ['Liliana'],
+  supertypes: ['Legendary'],
+  text:
+    '+1: Each player discards a card.\n−2: Target player sacrifices a creature.\n−6: Separate all permanents target player controls into two piles. That player sacrifices all permanents in the pile of his or her choice.',
+  type: 'Legendary Planeswalker — Liliana',
+  types: ['Planeswalker'],
+};
+
+export const snapMage = {
+  multiverseid: 272709,
+  artist: 'Joseph Meehan',
+  cmc: 2,
+  colorIdentity: ['U'],
+  colors: ['Blue'],
+  foreignNames: [
+    {
+      language: 'Chinese Simplified',
+      name: '迅咒法师',
+    },
+    {
+      language: 'Chinese Traditional',
+      name: '迅咒法師',
+    },
+    {
+      language: 'French',
+      name: 'Mage lancevif',
+    },
+    {
+      language: 'German',
+      name: 'Schnellzauberer',
+    },
+    {
+      language: 'Italian',
+      name: 'Mago Lanciorapido',
+    },
+    {
+      language: 'Japanese',
+      name: '瞬唱の魔道士',
+    },
+    {
+      language: 'Portuguese (Brazil)',
+      name: 'Mago da Conjuração-relâmpago',
+    },
+    {
+      language: 'Russian',
+      name: 'Маг Молниеносных Заклинаний',
+    },
+    {
+      language: 'Spanish',
+      name: 'Mago lanzachasquidos',
+    },
+  ],
+  id: '4f5c2cc0802213405529455dbfc76563b3fc5267',
+  imageName: 'snapcaster mage',
+  layout: 'normal',
+  legalities: [
+    {
+      format: 'Commander',
+      legality: 'Legal',
+    },
+    {
+      format: 'Innistrad Block',
+      legality: 'Legal',
+    },
+    {
+      format: 'Legacy',
+      legality: 'Legal',
+    },
+    {
+      format: 'Modern',
+      legality: 'Legal',
+    },
+    {
+      format: 'Vintage',
+      legality: 'Legal',
+    },
+  ],
+  manaCost: '{1}{U}',
+  mciNumber: '5',
+  name: 'Snapcaster Mage',
+  number: '5',
+  power: '2',
+  printings: ['ISD', 'pWCQ', 'MM3'],
+  rarity: 'Special',
+  rulings: [
+    {
+      date: '2017-03-14',
+      text:
+        'If you cast an instant or sorcery with {X} in its mana cost this way, you still choose the value of X as part of casting the spell and pay that cost.',
+    },
+    {
+      date: '2017-03-14',
+      text:
+        'If an instant or sorcery card in your graveyard already has flashback, you may use either flashback ability to cast it from your graveyard.',
+    },
+    {
+      date: '2017-03-14',
+      text:
+        'You may pay any optional additional costs the spell has, such as conspire costs. You must pay any mandatory additional costs the spell has, such as that of Bone Splinters.',
+    },
+    {
+      date: '2017-04-18',
+      text:
+        'For split cards, the flashback cost you pay is determined by the half you cast',
+    },
+  ],
+  source: 'Regional PTQ 2016',
+  subtypes: ['Human', 'Wizard'],
+  text:
+    'Flash\nWhen Snapcaster Mage enters the battlefield, target instant or sorcery card in your graveyard gains flashback until end of turn. The flashback cost is equal to its mana cost. (You may cast that card from your graveyard for its flashback cost. Then exile it.)',
+  toughness: '1',
+  type: 'Creature — Human Wizard',
+  types: ['Creature'],
+};
+
+export const cards = [lordOfAtlantis, liliana, snapMage];
