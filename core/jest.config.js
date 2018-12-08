4db46970f7f3c452694f13c543e84e66f15d58c0
Eduardo Moroni <eduardomoroni@gmail.com>
2018-12-08 09:48:00 +0100

    fix: core para babel 7

--- core/jest.config.js
+++ core/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   displayName: 'DOMAIN',
   coverageDirectory: '<rootDir>/coverage/',
   testMatch: ['**/__tests__/*.test.js?(x)'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
 };
