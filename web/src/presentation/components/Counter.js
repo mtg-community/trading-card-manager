282073f59bfb78004c7fed24a6f29474b82cf92c
LeoMosiah <leonardomosiah@gmail.com>
2018-12-08 08:20:55 -0200

    counter presentation refatorado

--- web/src/presentation/components/Counter.js
+++ web/src/presentation/components/Counter.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-function Counter(props) {
-  const { handleDecrement, handleIncrement, counter } = props;
-  return (
-    <React.Fragment>
-      <button onClick={handleIncrement}>Increment</button>
-      <p>{counter}</p>
-      <button onClick={handleDecrement}>Decrement</button>
-    </React.Fragment>
-  );
+export const CounterComponent = ({ handleDecrement, handleIncrement, counter }) => {
+    return (
+        <React.Fragment>
+            <button onClick={handleIncrement}>Increment</button>
+            <p>{counter}</p>
+            <button onClick={handleDecrement}>Decrement</button>
+        </React.Fragment>
+    );
 }
-
-export default Counter;
