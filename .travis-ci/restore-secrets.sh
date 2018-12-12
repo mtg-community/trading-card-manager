205d3a3807db73844cfa3aa7b58d61200c690525
Eduardo Moroni <eduardomoroni@gmail.com>
2018-12-12 08:17:30 +0100

    ci: add secret key

--- .travis-ci/restore-secrets.sh
+++ .travis-ci/restore-secrets.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env sh
 # https://docs.travis-ci.com/user/encrypting-files/
 
+REPO_SSH_URL="git@bitbucket.org:eduardomoroni/trading-card-manager-secrets.git"
+SECRETS_PATH="trading-card-manager-secrets/secrets"
+KEY_PATH=".travis-ci/bitbucket_key"
+
 echo "Decrypting bitbucket ssh key..."
-openssl aes-256-cbc -K $encrypted_385968bf931c_key \
-  -iv $encrypted_385968bf931c_iv \
-  -in .travis-ci/bitbucket_key.enc \
-  -out .travis-ci/bitbucket_key -d
+openssl aes-256-cbc \
+  - k $bitbucket_key \
+  # -K $encrypted_385968bf931c_key \
+  # -iv $encrypted_385968bf931c_iv \
+  -in "$KEY_PATH.enc" \
+  -out "$KEY_PATH" -d
 
 echo "Setting up SSH keys..."
 eval "$(ssh-agent)"
-chmod 600 .travis-ci/bitbucket_key
-ssh-add .travis-ci/bitbucket_key
+chmod 600 "$KEY_PATH"
+ssh-add "$KEY_PATH"
 
 echo "Cloning secrets repository..."
-REPO_SSH_URL="git@bitbucket.org:eduardomoroni/trading-card-manager-secrets.git"
-SECRETS_PATH="trading-card-manager-secrets/secrets"
 git clone --depth 1 "$REPO_SSH_URL"
-mv "$SECRETS_PATH/google-services.json" mobile/android/app/google-services.json
-mv "$SECRETS_PATH/GoogleService-Info.plist" mobile/ios/GoogleService-Info.plist
+
+echo "Moving the secrets to the right folder..."
+mv "$SECRETS_PATH/google-services.json" \
+  mobile/android/app/google-services.json
+mv "$SECRETS_PATH/GoogleService-Info.plist" \
+  mobile/ios/GoogleService-Info.plist
 mv "$SECRETS_PATH/.env" mobile/.env
