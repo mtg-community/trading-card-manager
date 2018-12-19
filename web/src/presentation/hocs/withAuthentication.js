abd3d0db7317d0a12e9e613727ab96b0fe5ff8a4
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-12-19 10:42:34 -0200

    refatorando hoc (#40)

--- web/src/presentation/hocs/withAuthentication.js
+++ web/src/presentation/hocs/withAuthentication.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {  selectUser } from 'core';
-import { compose } from 'redux';
 import { Redirect } from 'react-router-dom';
-
-export const authenticationHoc = Component => ({user}) => {
-  return user ? <Component user={user}/> : <Redirect to="/signIn"/>
-};
 
 const mapStateToProps = state => ({
   user: selectUser(state),
 });
 
-export const withAuthentication = compose(
-  connect(mapStateToProps),
-  authenticationHoc
-);
+export const withAuthentication = Component =>  {
+  const componentWithAuthenticationChecker = ({user}) => (
+     user ? <Component user ={user}/> : <Redirect to="/signIn"/>
+  );
+  return connect (mapStateToProps)(componentWithAuthenticationChecker)
+};
