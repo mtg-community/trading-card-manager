f6eb2e4b193c9dbe50c26346f8ea4150dac17312
Eduardo Moroni <eduardomoronI@gmail.com>
2018-12-21 12:15:01 +0100

    Adiciona versao custom do CRA (#50)
    * feat: add default parameter
    
    * build: adiciona versao custom do create-react-app

--- web/src/domain/index.js
+++ web/src/domain/index.js
@@ -1,29 +1,26 @@
 import {
   ReduxAdapter,
   AuthenticationInteractor,
-  CounterInteractor,
   configureStore,
 } from 'core';
 import {
-  sendPasswordResetEmail,
+  onAuthStateChanged, sendPasswordResetEmail,
   signInWithEmailAndPassword,
   signOut,
   signUpWithEmailAndPassword,
 } from '../data/firebase/authentication';
 
 export const initializeDomainLayer = () => {
-  const counterInteractor = new CounterInteractor(-5, 5);
   const authenticationInteractor = new AuthenticationInteractor({
     signOut: signOut,
     signIn: signInWithEmailAndPassword,
     signUp: signUpWithEmailAndPassword,
     resetPassword: sendPasswordResetEmail,
-    authStateListener: () => ({}),
+    authStateListener: onAuthStateChanged,
   });
 
   const reduxAdapter = new ReduxAdapter(
     authenticationInteractor,
-    counterInteractor,
   );
 
   const store = configureStore(reduxAdapter);
