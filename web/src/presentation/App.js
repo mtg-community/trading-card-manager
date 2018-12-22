160428b3d117362e8be9a3d62645c1776929d21d
LeoMosiah <leonardomosiah@gmail.com>
2018-12-22 20:48:35 -0200

    lifecicle componentDidMount implementado

--- web/src/presentation/App.js
+++ web/src/presentation/App.js
@@ -4,13 +4,19 @@ import { Router } from './Router';
 import { Provider } from 'react-redux';
 import './App.css';
 import { onAuthStateChanged } from '../data/firebase/authentication';
+import {setUserAction, User} from 'core';
 
 export class App extends Component<{}> {
 
   unsubscribe = () => {};
 
+  setUser(user){
+    const authedUser = new User(user.email,user.emailVerified);
+    this.props.store.dispatch(setUserAction(authedUser))
+  }
+
   componentDidMount() {
-    this.unsubscribe = onAuthStateChanged((user) => console.log(user), console.error, console.info)
+    this.unsubscribe = onAuthStateChanged((user) => this.setUser(user), console.error, console.info)
   }
 
   componentWillUnmount () {
