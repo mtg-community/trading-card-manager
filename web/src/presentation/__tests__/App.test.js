26edc08bfa8fff3494f09c3c13ace1dc972d436f
LeoMosiah <leonardomosiah@gmail.com>
2018-12-27 15:14:29 -0200

    acertando test

--- web/src/presentation/__tests__/App.test.js
+++ web/src/presentation/__tests__/App.test.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { initializeDomainLayer } from '../../domain';
-import { allMessages } from '../internalization/messages';
-import { App } from '../App';
+import { App } from '../containers/App';
+import { initializeInternalization } from '../internalization';
+import {Provider} from "react-redux";
+
 
 jest.mock('../../data/firebase/authentication', () => ({
   onAuthStateChanged: jest.fn(() => () => {}),
 }));
 const { store } = initializeDomainLayer();
+const { messages } = initializeInternalization();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App store={store} messages={allMessages} />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App messages={messages} />
+    </Provider>
+      , div);
   ReactDOM.unmountComponentAtNode(div);
 });
