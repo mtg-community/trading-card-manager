745641137be5c70d033904914344e120f22cfaec
Eduardo Moroni <eduardomoroni@gmail.com>
2018-12-29 08:26:11 +0100

    feat: nao aceita undefined como user, ao inves converte para null

--- core/src/frameworks/redux/ducks/userReducer.js
+++ core/src/frameworks/redux/ducks/userReducer.js
@@ -57,12 +57,12 @@ export const forgotPasswordAction = (
 
 export const selectUser = (state: StateType): ?User => state.user;
 
-export const setUserAction = (user: ?User): SetUserAction => ({
+export const setUserAction = (user: ?User = null): SetUserAction => ({
   type: SET_USER,
   user,
 });
 
-export const setUserListenerAction = (user: ?User): SetUserAction => ({
+export const setUserListenerAction = (user: ?User = null): SetUserAction => ({
   type: SET_USER_LISTENER,
   user,
 });
