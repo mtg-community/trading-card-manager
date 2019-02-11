d0fc53c38d03acb0e9f86f4cb51b9156f49898e2
Eduardo Moroni <eduardomoroni@gmail.com>
2019-02-11 20:31:02 +0100

    refactor: first hook

--- web/src/presentation/App.js
+++ web/src/presentation/App.js
@@ -1,46 +1,53 @@
+import { User } from 'core/src/entities';
+import { selectUser } from 'core/src/frameworks/redux/ducks/userReducer';
 import { onAuthStateChanged } from '../data/firebase/authentication';
 import { IntlProvider } from 'react-intl';
 import { Router } from './Router';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { localeSelector } from '../domain/redux/ducks/localeReducer';
 import { whenUserLogsIn, whenUserLogsOut } from '../domain/useCases/user';
 
-class AppContainer extends Component {
-  unsubscribe = () => {};
-
-  userHasChangedCallback = user => {
-    if (user) {
-      whenUserLogsIn(user);
-    } else {
-      whenUserLogsOut();
-    }
-  };
-
-  componentDidMount() {
-    this.unsubscribe = onAuthStateChanged(this.userHasChangedCallback);
+const userHasChangedCallback = user => {
+  if (user) {
+    whenUserLogsIn(user);
+  } else {
+    whenUserLogsOut();
   }
+};
 
-  render() {
-    const { messages, locale } = this.props;
+const AppContainer = props => {
+  const { messages, locale, user } = props;
 
-    return (
-      <IntlProvider key={locale} locale={locale} messages={messages[locale]}>
-        <Router />
-      </IntlProvider>
-    );
-  }
-}
+  useEffect(
+    () => {
+      const unSubscriber = onAuthStateChanged(userHasChangedCallback);
+
+      return function componentWillUnmount() {
+        return unSubscriber();
+      };
+    },
+    [user],
+  );
+
+  return (
+    <IntlProvider key={locale} locale={locale} messages={messages[locale]}>
+      <Router />
+    </IntlProvider>
+  );
+};
 
 const mapStateToProps = state => ({
   locale: localeSelector(state),
+  user: selectUser(state),
 });
 
 AppContainer.propTypes = {
   messages: PropTypes.object.isRequired,
   locale: PropTypes.string.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 AppContainer.displayName = 'App';
--- web/src/presentation/hocs/withAuthentication.js
+++ web/src/presentation/hocs/withAuthentication.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {  selectUser } from 'core';
+import { selectUser } from 'core';
 import { Redirect } from 'react-router-dom';
 
 const mapStateToProps = state => ({
   user: selectUser(state),
 });
 
-export const withAuthentication = Component =>  {
-  const componentWithAuthenticationChecker = ({user}) => (
-     user ? <Component user ={user}/> : <Redirect to="/signIn"/>
-  );
-  return connect (mapStateToProps)(componentWithAuthenticationChecker)
+export const withAuthentication = Component => {
+  const componentWithAuthenticationChecker = ({ user }) =>
+    user ? <Component user={user} /> : <Redirect to="/signIn" />;
+  return connect(mapStateToProps)(componentWithAuthenticationChecker);
 };
