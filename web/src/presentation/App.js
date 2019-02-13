a8d145c7a44979e028c5c8f5bef22a757cfd91ef
Eduardo Moroni <eduardomoroni@gmail.com>
2019-02-13 08:59:13 +0100

    fix: package issue

--- web/src/presentation/App.js
+++ web/src/presentation/App.js
@@ -1,13 +1,15 @@
-import { selectUser } from 'core/src/frameworks/redux/ducks/userReducer';
-import { onAuthStateChanged } from '../data/firebase/authentication';
-import { IntlProvider } from 'react-intl';
-import { Router } from './Router';
-import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
-import './App.css';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { IntlProvider } from 'react-intl';
+import { selectUser } from 'core';
+
+import { onAuthStateChanged } from '../data/firebase/authentication';
+import { Router } from './Router';
 import { localeSelector } from '../domain/redux/ducks/localeReducer';
 import { whenUserLogsIn, whenUserLogsOut } from '../domain/useCases/user';
+
+import './App.css';
 
 const userHasChangedCallback = user => {
   if (user) {
@@ -20,16 +22,13 @@ const userHasChangedCallback = user => {
 const AppContainer = props => {
   const { messages, locale, user } = props;
 
-  useEffect(
-    () => {
-      const unSubscriber = onAuthStateChanged(userHasChangedCallback);
+  useEffect(() => {
+    const unSubscriber = onAuthStateChanged(userHasChangedCallback);
 
-      return function componentWillUnmount() {
-        return unSubscriber();
-      };
-    },
-    [user],
-  );
+    return function componentWillUnmount() {
+      return unSubscriber();
+    };
+  }, [user]);
 
   return (
     <IntlProvider key={locale} locale={locale} messages={messages[locale]}>
