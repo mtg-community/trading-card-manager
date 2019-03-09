4a8562052a3e06f7bb65c4a651ba9c1472383933
Eduardo Moroni <eduardomoroni@gmail.com>
2019-03-09 19:48:20 +0100

    test: improve test

--- web/src/core/frameworks/redux/store.test.js
+++ web/src/core/frameworks/redux/store.test.js
@@ -1,11 +1,19 @@
 import { configureStore } from './store';
 import { mockReduxAdapter } from './__mocks__/reduxAdapter';
+import { ReduxAdapter } from './reduxAdapter';
+import { mockAuthenticationInteractor } from '../../useCases/__mocks__/authenticatorInteractor';
 
 describe('Store', () => {
   it('fails when initialized without ReduxAdapter', () => {
     expect(() => {
       configureStore();
     }).toThrow();
+
+    expect(() => {
+      const mockReduxAdapter = new ReduxAdapter(mockAuthenticationInteractor);
+      mockReduxAdapter.hasBeenInitialized = () => false;
+      configureStore(mockReduxAdapter);
+    }).toThrow('ReduxAdapter is required.');
   });
 
   it('configures and creates the store', () => {
