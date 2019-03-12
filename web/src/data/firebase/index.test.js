5258dd292544fe260f5db5d6f3dbeb6ad8e23ee7
Eduardo Moroni <eduardomoroni@gmail.com>
2019-03-12 10:20:28 +0100

    test: firebase initialization

--- web/src/data/firebase/index.test.js
+++ web/src/data/firebase/index.test.js
@@ -1,30 +1,36 @@
+import cloneDeep from 'lodash/cloneDeep';
+import { initializeFirebase } from './index';
 import firebase from 'firebase/app';
-import 'firebase/auth';
+jest.mock('firebase/app');
 
-const ERROR = {
-  duplicatedApp: {
-    code: 'app/duplicate-app',
-    message:
-      'Hot reload tried to initiate firebase again. Ignoring duplicated initialization',
-  },
-};
-
-export const initializeFirebase = () => {
-  const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+describe('Firebase initialization', function() {
+  const env = cloneDeep(process.env);
+  const envVarsToInject = {
+    REACT_APP_FIREBASE_API_KEY: 'REACT_APP_FIREBASE_API_KEY',
+    REACT_APP_FIREBASE_AUTH_DOMAIN: 'REACT_APP_FIREBASE_AUTH_DOMAIN',
+    REACT_APP_FIREBASE_DATABASE_URL: 'REACT_APP_FIREBASE_DATABASE_URL',
+    REACT_APP_FIREBASE_STORAGE_BUCKET: 'REACT_APP_FIREBASE_STORAGE_BUCKET',
+    REACT_APP_FIREBASE_MSG_SENDER_ID: 'REACT_APP_FIREBASE_MSG_SENDER_ID',
+    REACT_APP_FIREBASE_PROJECT_ID: 'REACT_APP_FIREBASE_PROJECT_ID',
   };
-  try {
-    firebase.initializeApp(config);
-  } catch (error) {
-    if (error.code === ERROR.duplicatedApp.code) {
-      console.info(ERROR.duplicatedApp.message);
-    } else {
-      throw error;
-    }
-  }
-};
+
+  afterAll(() => {
+    process.env = env;
+  });
+
+  it('initializes using the correct env vars', function() {
+    process.env = { ...env, ...envVarsToInject };
+    const expectedConfig = {
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    };
+
+    initializeFirebase();
+
+    expect(firebase.initializeApp).toHaveBeenCalledWith(expectedConfig);
+  });
+});
