bdd89b0ec1a1daf79e2fb8e845b7bdcad2babea9
Eduardo Moroni <eduardomoroni@gmail.com>
2019-03-23 13:31:30 +0100

    refactor: move things under lerna folder structure

---
+++ packages/web/src/presentation/components/SignUp.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const SignUpComponent = ({
+  handleEmail,
+  handlePassword,
+  handleSubmit,
+  password,
+  email,
+}) => {
+  return (
+    <form onSubmit={event => handleSubmit(event)}>
+      <input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={event => handleEmail(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={event => handlePassword(event.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+SignUpComponent.propTypes = {
+  handleEmail: PropTypes.func.isRequired,
+  handlePassword: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  password: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
+SignUpComponent.displayName = 'SignUp';
---
+++ packages/web/src/presentation/internalization/messages/pt.js
@@ -0,0 +1,5 @@
+export const messagesPt = {
+  'private.title': 'Acessivel apenas para',
+  'modal.title': 'Modal do Counter',
+  'modal.content': 'Conteudo do Modal',
+};
