59556904791e73106bc134dbb501250dcec8b5c5
LeoMosiah <leonardomosiah@gmail.com>
2019-04-16 17:02:17 -0300

    fix: bitbucket key

--- .travis-ci/restore-secrets.sh
+++ .travis-ci/restore-secrets.sh
@@ -3,13 +3,16 @@
 
 REPO_SSH_URL="git@bitbucket.org:eduardomoroni/trading-card-manager-secrets.git"
 SECRETS_PATH="trading-card-manager-secrets/mobile"
-KEY_PATH=".travis-ci/bitbucket_key.pub"
+KEY_PATH=".travis-ci/bitbucket_key"
 KEY_ENCODING_EXTENSION=".enc"
+
+echo "Decrypting bitbucket ssh key..."
+openssl aes-256-cbc -K $encrypted_43f02176fa37_key -iv $encrypted_43f02176fa37_iv -in bitbucket_key.enc -out bitbucket_key -d
 
 echo "Setting up SSH keys..."
 eval "$(ssh-agent)"
-chmod 600 "$KEY_PATH$KEY_ENCODING_EXTENSION"
-ssh-add "$KEY_PATH$KEY_ENCODING_EXTENSION"
+chmod 600 "$KEY_PATH"
+ssh-add "$KEY_PATH"
 
 echo "Cloning secrets repository..."
 git clone --depth 1 "$REPO_SSH_URL"
