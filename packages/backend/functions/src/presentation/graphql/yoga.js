e0a7d50558d6107474e7281f263377fb36efd49e
Eduardo Moroni <eduardomoroni@gmail.com>
2019-04-29 14:27:52 +0200

    feat: add context

--- packages/backend/functions/src/presentation/graphql/yoga.js
+++ packages/backend/functions/src/presentation/graphql/yoga.js
@@ -1,12 +1,13 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { typeDefs } = require('./typeDefs');
 const { queries } = require('./queries');
+const { context } = require('./context');
 
 const resolvers = {
   Query: queries,
 };
 
-const graphQLServer = new GraphQLServer({ typeDefs, resolvers });
+const graphQLServer = new GraphQLServer({ typeDefs, resolvers, context });
 graphQLServer.createHttpServer({
   cors: true,
 });
