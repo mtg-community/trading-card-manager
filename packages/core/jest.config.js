54eacb903dbbb8ac68d2f6aee3bc30262c73dd86
Eduardo Moroni <eduardomoroni@gmail.com>
2019-05-27 23:05:45 +0200

    test: configure jest

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   coverageDirectory: '<rootDir>/coverage/',
   testMatch: ['**/__tests__/*.test.(js|ts)?(x)'],
   collectCoverage: process.env.CI === true,
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
