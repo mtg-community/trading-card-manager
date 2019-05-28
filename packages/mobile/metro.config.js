a6b7338a17d680904de96276f03ec3cad852b25e
Eduardo Moroni <eduardomoroni@gmail.com>
2019-05-28 09:53:16 +0200

    fix: failed attempt on mobile

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -4,15 +4,15 @@ const projectRoot = __dirname;
 
 module.exports = {
   //https://github.com/facebook/metro/issues/1#issuecomment-421628147, you can also try an alternative solution -> // https://github.com/facebook/metro/issues/1#issuecomment-448064559
-  watchFolders: Array.from(
-    new Set(getDevPaths(projectRoot).map($ => fs.realpathSync($))),
-  ),
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
+  // watchFolders: Array.from(
+  //   new Set(getDevPaths(projectRoot).map($ => fs.realpathSync($))),
+  // ),
+  // transformer: {
+  //   getTransformOptions: async () => ({
+  //     transform: {
+  //       experimentalImportSupport: false,
+  //       inlineRequires: false,
+  //     },
+  //   }),
+  // },
 };
