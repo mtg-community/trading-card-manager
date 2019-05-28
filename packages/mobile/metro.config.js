dec2cc3f97817eb2ead2cc4d02dc4e24ad948908
Eduardo Moroni <eduardomoroni@gmail.com>
2019-05-28 10:39:45 +0200

    feat: mobile is working

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -2,17 +2,21 @@ const fs = require('fs');
 const getDevPaths = require('get-dev-paths');
 const projectRoot = __dirname;
 
+//https://github.com/facebook/metro/issues/1#issuecomment-421628147, you can also try an alternative solution -> // https://github.com/facebook/metro/issues/1#issuecomment-448064559
+const watchFolders = Array.from(
+  new Set(getDevPaths(projectRoot).map($ => fs.realpathSync($))),
+);
+
 module.exports = {
-  //https://github.com/facebook/metro/issues/1#issuecomment-421628147, you can also try an alternative solution -> // https://github.com/facebook/metro/issues/1#issuecomment-448064559
-  // watchFolders: Array.from(
-  //   new Set(getDevPaths(projectRoot).map($ => fs.realpathSync($))),
-  // ),
-  // transformer: {
-  //   getTransformOptions: async () => ({
-  //     transform: {
-  //       experimentalImportSupport: false,
-  //       inlineRequires: false,
-  //     },
-  //   }),
-  // },
+  watchFolders,
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
 };
+
+console.log(JSON.stringify(watchFolders));
