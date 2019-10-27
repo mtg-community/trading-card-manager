b159139a4134f1a3a12e19c291ebf50402321d22
Eduardo Moroni <eduardomoroni@gmail.com>
2019-10-27 17:25:31 +0100

    fix: lint

--- packages/frontend/prettier.config.js
+++ packages/frontend/prettier.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  parser: 'typescript'
+  parser: "typescript"
 };
--- packages/frontend/src/presentation/components/Input.tsx
+++ packages/frontend/src/presentation/components/Input.tsx
@@ -3,9 +3,7 @@ import { View, TextInput, TextInputProps } from 'react-native';
 import { styles } from './styles/Input.styles';
 import { Colors } from '../constants/colors';
 
-interface InputProps extends TextInputProps {}
-
-export const Input: React.FC<InputProps> = ({ ...props }) => (
+export const Input: React.FC<TextInputProps> = ({ ...props }) => (
   <View style={styles.container}>
     <TextInput
       style={styles.input}
