33fa0ae5350a07f19d1fde1e6d52e73ae1d42206
Leonardo Mosiah <leonardomosiah@gmail.com>
2019-10-28 10:36:40 -0300

    removing-unnecessary-index-file

--- packages/frontend/index.ts
+++ packages/frontend/index.ts
@@ -1,9 +1,15 @@
 import { registerRootComponent } from 'expo';
 import { activateKeepAwake } from 'expo-keep-awake';
-import { initializeApplication } from './src';
+import { initializeDataLayer } from './src/data';
+import { initializeDomainLayer } from './src/domain';
+import { initializePresentationLayer } from './src/presentation';
+
+const { BugSnag } = initializeDataLayer();
+const store = initializeDomainLayer(BugSnag);
 
 if (__DEV__) {
   activateKeepAwake();
 }
 
-registerRootComponent(initializeApplication());
+registerRootComponent(initializePresentationLayer(store));
+
--- packages/frontend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { initializeDataLayer } from './data';
-import { initializeDomainLayer } from './domain';
-import { initializePresentationLayer } from './presentation';
-
-export const initializeApplication = () =>  {
-  const { BugSnag } = initializeDataLayer();
-  const store = initializeDomainLayer(BugSnag);
-  return initializePresentationLayer(store);
-}
