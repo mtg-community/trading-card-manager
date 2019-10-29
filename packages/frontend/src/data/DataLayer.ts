c93a4349ea7d7e4271b551cd13440ea2dcad58c9
Eduardo Moroni <eduardomoroni@gmail.com>
2019-10-29 13:58:12 +0100

    feat: add graphql

--- packages/frontend/src/data/DataLayer.ts
+++ packages/frontend/src/data/DataLayer.ts
@@ -1,20 +1,17 @@
 import { initializeFirebase } from './firebase';
 import { initBugSnag, BugSnag } from './bugsnag';
 import { initApolloClient } from './graphql/Apollo';
-import ApolloClient from 'apollo-client';
 
 type DataLayerInit = {
   BugSnag: BugSnag;
-  ApolloClient: ApolloClient<unknown>;
 };
 
 export function initializeDataLayer(): DataLayerInit {
   initializeFirebase();
   const BugSnag = initBugSnag();
-  const ApolloClient = initApolloClient();
+  initApolloClient();
 
   return {
     BugSnag,
-    ApolloClient,
   };
 }
--- packages/frontend/src/data/graphql/queries/SampleCardList.ts
+++ packages/frontend/src/data/graphql/queries/SampleCardList.ts
@@ -2,6 +2,7 @@ import { gql } from 'apollo-boost';
 import { apolloClient } from '../Apollo';
 import { Card as ApiCard } from '../../../../types/graphql-api';
 import { Card } from '../../../domain/entities/Card';
+import { omit } from 'ramda';
 
 const sampleCardQuery = gql`
   query {
@@ -50,59 +51,7 @@ interface SampleCardQueryData {
 }
 
 function mapApiResponseToDomainModel(response: ApiCard): Card {
-  const {
-    id,
-    uuid,
-    artist,
-    colorIdentities,
-    colors,
-    layout,
-    name,
-    number,
-    power,
-    toughness,
-    text,
-    type,
-    watermark,
-    convertedManaCost,
-    flavorText,
-    foreignData,
-    manaCost,
-    multiverseId,
-    rarity,
-    rulings,
-    printings,
-    subTypes,
-    superTypes,
-    types,
-  } = response;
-
-  return {
-    id,
-    uuid,
-    artist,
-    colorIdentities,
-    colors,
-    layout,
-    name,
-    number,
-    power,
-    toughness,
-    text,
-    type,
-    watermark,
-    convertedManaCost,
-    flavorText,
-    foreignData,
-    manaCost,
-    multiverseId,
-    rarity,
-    rulings,
-    printings,
-    subTypes,
-    superTypes,
-    types,
-  };
+  return omit(['__typename'], response);
 }
 
 export async function querySampleCard(): Promise<Card> {
