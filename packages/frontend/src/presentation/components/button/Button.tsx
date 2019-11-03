2200405fbbed557c00a6e64c6d5e95e9199395e4
Eduardo Moroni <eduardomoroni@gmail.com>
2019-11-03 13:57:28 +0100

    feat: overengineer button

--- packages/frontend/src/presentation/components/button/Button.tsx
+++ packages/frontend/src/presentation/components/button/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { useSetStateIfMounted } from '../../hooks/useSetStateIfMounted';
 
 interface ButtonProps {
   onPress: () => void;
@@ -15,10 +16,36 @@ const styles = StyleSheet.create({
   },
 });
 
+enum ButtonState {
+  IDLE,
+  BUSY,
+  FAILED,
+}
+
 export const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
+  const [state, setState] = useSetStateIfMounted<ButtonState>(ButtonState.IDLE);
+
+  async function avoidClickingWhenPromiseIsResolving(): Promise<void> {
+    try {
+      if (state !== ButtonState.BUSY) {
+        setState(ButtonState.BUSY);
+        await props.onPress();
+        setState(ButtonState.IDLE);
+      }
+    } catch (e) {
+      setState(ButtonState.FAILED);
+      console.info('Failed to execute button handler:');
+      console.error(e);
+    }
+  }
+
   return (
-    <TouchableOpacity style={styles.container} onPress={props.onPress}>
-      <Text>{props.label}</Text>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={avoidClickingWhenPromiseIsResolving}
+    >
+      <Text>{state === ButtonState.BUSY ? 'Wait' : props.label}</Text>
+      {state === ButtonState.FAILED ? <Text>FAILED</Text> : null}
     </TouchableOpacity>
   );
 };
