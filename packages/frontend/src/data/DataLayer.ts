4b2e24e0a403e2773f9c7479078af005998cc9b0
Leonardo Mosiah <leonardomosiah@gmail.com>
2019-12-17 09:22:54 -0300

    expo-sdk-36

--- packages/frontend/src/data/DataLayer.ts
+++ packages/frontend/src/data/DataLayer.ts
@@ -1,5 +1,7 @@
+import { initializeAsync } from 'expo-facebook';
+import { FACEBOOK_APP_ID } from 'react-native-dotenv';
 import { initializeFirebase } from './firebase';
-import { initBugSnag, BugSnag } from './bugsnag';
+import { BugSnag, initBugSnag } from './bugsnag';
 import { initApolloClient } from './graphql/Apollo';
 import { initializeAxios } from './services';
 
@@ -9,8 +11,10 @@ type DataLayerInit = {
 
 export function initializeDataLayer(): DataLayerInit {
   let BugSnag = undefined;
+  const APP_NAME = 'mtgx frontend';
 
   try {
+    initializeAsync(FACEBOOK_APP_ID, APP_NAME);
     initializeFirebase();
     initApolloClient();
     initializeAxios();
